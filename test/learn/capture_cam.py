b4f77063f1b83bd1f2f94c6d11d0239b1579ff72
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-07-20 10:21:01 +0530

    Delete old files

--- test/learn/capture_cam.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-cap = cv2.imread('3.png',1)
-
-cv2.imshow('Kodaikanal',cap)
-
-cv2.waitKey(0)
--- test/learn/fac.py
+++
@@ -1,3 +0,0 @@
-import face_recognition
-
-print("Face Recognition library is working correctly!")
